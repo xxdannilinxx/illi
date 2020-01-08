a8e2e72157957aabe5dcbfd71e14f0a7ee8987c1
Danilo <danilo@illimitar.com.br>
2020-01-08 11:28:11 -0300

    Commit

--- Prova/visual.php
+++ Prova/visual.php
@@ -9,7 +9,7 @@ $this->view('relatorios/cabecalho');
         <th style="width: 25%;text-align: center;">Plano de Contas </th>
         <?php
         /**
-        * Array de títulos, das entidades
+        * Array de títulos da tabela, com as entidades
         */
         foreach($entidades as $entidade) {
          echo '<th style="width: 25%;text-align: center;">'.$entidade.'</th>';
@@ -23,20 +23,19 @@ $this->view('relatorios/cabecalho');
         foreach($fluxos as $fluxo) {
             $temp = [];
             /**
-            * Lista os fluxos de acordo com a classificação
+            * Lista os fluxos de acordo com a classificação da natureza de lançamento
             */
             echo '<tr>';
             echo '<td style="width: 10%;text-align: left;">'.$fluxo['classificacao'].'</td>';
             echo '<td style="width: 25%;text-align: left;">'.$fluxo['descricao'].'</td>';
             /**
-             * Repetição da lista de valores e entidades
+             * Repetição da lista de preços e entidades
              */
             $precos = preg_split('/[,]/', preg_replace('/[][]/', '', $fluxo['array_entidade']));
             foreach($precos as $preco) {
                 $preco = preg_split('/[=>]/', $preco);
                 /**
-                * Explode a entidade e seu devido valor
-                * Insere no array de entidades o preço e verifica se já não foi alterado anteriormente
+                * Explode a entidade e seu devido preço, inserindo no array temporário
                 */
                 $temp[$preco[0]] = $preco[2];
             }
